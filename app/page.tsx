45c6e33bc852009c9071716305cddafa1dc23ecd
vandilsonbrito <vandilson25@outlook.com>
2024-11-20 18:39:14 -0300

    feat: add 'TreeMap' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import TreeMap from "@/components/TreeMap";
 
 export default function Home() {
 
   return (
     <div className="w-full h-full min-h-screen bg-background">
         <Header/>
+        <TreeMap/>
     </div>
   );
 }
