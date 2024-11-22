5adf3bdea0f3523690257a1661c19908e6289bcf
vandilsonbrito <vandilson25@outlook.com>
2024-11-22 17:15:57 -0300

    feat add 'ModalSecction' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import Header from "@/components/Header";
 import AsideSection from "@/components/AsideSection";
+import ModalSection from "@/components/ModalSection";
 
 const TreeMap = dynamic(() => import("../components/TreeMap"), { ssr: false });
 
@@ -9,9 +10,10 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <main className="w-full h-full min-h-screen bg-background text-white overflow-hidden relative">
+      <main className="w-full h-full min-h-screen bg-background text-white overflow-hidden relative z-0">
           <TreeMap/>
           <AsideSection/>
+          <ModalSection/>
       </main>
     </>
   );
