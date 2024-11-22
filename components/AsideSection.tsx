199e402e160a4741eb7dc4932de31b16fb7eb582
ramlow07 <luamramlow@gmail.com>
2024-11-21 23:08:26 -0300

    feat: consuming context from ZoomContext

--- components/AsideSection.tsx
+++ components/AsideSection.tsx
@@ -1,45 +1,58 @@
-'use client';
-import React from 'react';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import QuestionMarkTwoToneIcon from '@mui/icons-material/QuestionMarkTwoTone';
-import ProgressBar from './ProgressBar';
-import Tooltip from './Tooltip';
+"use client";
+import React from "react";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import QuestionMarkTwoToneIcon from "@mui/icons-material/QuestionMarkTwoTone";
+import ProgressBar from "./ProgressBar";
+import Tooltip from "./Tooltip";
+import { ZoomProvider } from "../contexts/ZoomContext";
+import { useZoomContext } from "../contexts/ZoomContext";
 
 export default function AsideSection() {
+  const { setZoom } = useZoomContext();
 
-    return (
-      <aside className='hidden lg:flex  w-[22%] h-[calc(100vh-1rem)] bg-customPurple text-white absolute top-2 bottom-2 right-2 overflow-hidden  flex-col items-center justify-between pt-5 pb-16 px-2'>
+  const handleResetZoom = () => {
+    setZoom(1); // Reset the zoom to its default value (1)
+  };
 
-          <div className="flex flex-col items-center gap-8">
-              <p>Select Roadmap</p>
-              <div className="w-full flex justify-center items-center">
-                <button className="px-6 py-[6px] rounded-2xl hover:bg-background bg-customPurpleBtn transition ease-in-out duration-300">Algorithms</button>
-                <button className="px-6 py-[6px] rounded-2xl hover:bg-background active:bg-customPurpleBtn transition ease-in-out duration-300">Courses</button>
-              </div>
-    
-              <div className="w-full mt-2 flex flex-col gap-2">
-                  <p className="text-center">(0 / 150)</p>
-                  <ProgressBar/>
-              </div>
-          </div>
+  return (
+    <aside className="hidden lg:flex  w-[22%] h-[calc(100vh-1rem)] bg-customPurple text-white absolute top-2 bottom-2 right-2 overflow-hidden  flex-col items-center justify-between pt-5 pb-16 px-2">
+      <div className="flex flex-col items-center gap-8">
+        <p>Select Roadmap</p>
+        <div className="w-full flex justify-center items-center">
+          <button className="px-6 py-[6px] rounded-2xl hover:bg-background bg-customPurpleBtn transition ease-in-out duration-300">
+            Algorithms
+          </button>
+          <button className="px-6 py-[6px] rounded-2xl hover:bg-background active:bg-customPurpleBtn transition ease-in-out duration-300">
+            Courses
+          </button>
+        </div>
 
-          <div className="w-full flex justify-between">
-            <Tooltip title={'Reset graph position'}>
-                <button className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300">
-                  Reset
-                </button>
-            </Tooltip>  
-            <Tooltip title={'What is this?'}>
-                <button className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300">
-                  <QuestionMarkTwoToneIcon/>
-                </button>  
-            </Tooltip>
-            <Tooltip title={'Settings'}>
-                <button className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300">
-                  <SettingsRoundedIcon/>
-                </button>  
-            </Tooltip>
-          </div>
-      </aside>
-    )
+        <div className="w-full mt-2 flex flex-col gap-2">
+          <p className="text-center">(0 / 150)</p>
+          <ProgressBar />
+        </div>
+      </div>
+
+      <div className="w-full flex justify-between">
+        <Tooltip title={"Reset graph position"}>
+          <button
+            onClick={() => handleResetZoom}
+            className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300"
+          >
+            Reset
+          </button>
+        </Tooltip>
+        <Tooltip title={"What is this?"}>
+          <button className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300">
+            <QuestionMarkTwoToneIcon />
+          </button>
+        </Tooltip>
+        <Tooltip title={"Settings"}>
+          <button className="px-6 py-[6px] rounded-2xl hover:bg-background transition ease-in-out duration-300">
+            <SettingsRoundedIcon />
+          </button>
+        </Tooltip>
+      </div>
+    </aside>
+  );
 }
--- components/TreeMap.tsx
+++ components/TreeMap.tsx
@@ -1,38 +1,39 @@
-'use client';
+"use client";
 import React from "react";
 import Tree from "react-d3-tree";
 import { useCenteredTree } from "../utils/TreeMapHelpers";
+import { useZoomContext } from "@/contexts/ZoomContext";
 
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
 const orgChart = {
-  name: 'CEO',
+  name: "CEO",
   children: [
     {
-      name: 'Manager',
+      name: "Manager",
       attributes: {
-        department: 'Production',
+        department: "Production",
       },
       children: [
         {
-          name: 'Foreman',
+          name: "Foreman",
           attributes: {
-            department: 'Fabrication',
+            department: "Fabrication",
           },
           children: [
             {
-              name: 'Worker',
+              name: "Worker",
             },
           ],
         },
         {
-          name: 'Foreman',
+          name: "Foreman",
           attributes: {
-            department: 'Assembly',
+            department: "Assembly",
           },
           children: [
             {
-              name: 'Worker',
+              name: "Worker",
             },
           ],
         },
@@ -41,12 +42,16 @@ const orgChart = {
   ],
 };
 
-
 // Here we're using `renderCustomNodeElement` render a component that uses
 // both SVG and HTML tags side-by-side.
 // This is made possible by `foreignObject`, which wraps the HTML tags to
 // allow for them to be injected into the SVG namespace.
-const renderForeignObjectNode = ({ nodeDatum, toggleNode, foreignObjectProps }) => (
+
+const renderForeignObjectNode = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps,
+}) => (
   <g>
     <circle r={15}></circle>
     {/* `foreignObject` requires width & height to be explicitly set. */}
@@ -55,7 +60,7 @@ const renderForeignObjectNode = ({ nodeDatum, toggleNode, foreignObjectProps }) 
         style={{
           border: "1px solid transparent",
           borderRadius: "5px",
-          backgroundColor: "var(--purpleLogo)"
+          backgroundColor: "var(--purpleLogo)",
         }}
       >
         <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
@@ -64,7 +69,7 @@ const renderForeignObjectNode = ({ nodeDatum, toggleNode, foreignObjectProps }) 
             style={{
               width: "75%",
               margin: "0 auto 1rem auto",
-              display: "block"
+              display: "block",
             }}
             onClick={toggleNode}
           >
@@ -77,18 +82,30 @@ const renderForeignObjectNode = ({ nodeDatum, toggleNode, foreignObjectProps }) 
 );
 
 export default function TreeMap() {
-    const [translate, containerRef] = useCenteredTree() as [{ x: number; y: number; }, (containerElem: { getBoundingClientRect: () => { width: number; height: number; }; } | null) => void];
+  const { zoom } = useZoomContext();
+
+  const [translate, containerRef] = useCenteredTree() as [
+    { x: number; y: number },
+    (
+      containerElem: {
+        getBoundingClientRect: () => { width: number; height: number };
+      } | null
+    ) => void
+  ];
   const { x, y } = translate;
 
-  const nodeSize = { x: 200, y: 200 };
+  const nodeSize = { x: 200, y: 150 };
   const foreignObjectProps = {
     width: nodeSize.x,
     height: nodeSize.y,
     x: -100,
-    y: -20
+    y: -20,
   };
   return (
-    <div ref={containerRef} className="w-[100vw] h-[100vh] lg:w-[76vw] text-white ">
+    <div
+      ref={containerRef}
+      className="w-[100vw] h-[100vh] lg:w-[76vw] text-white "
+    >
       <Tree
         data={orgChart}
         translate={{ x, y }}
@@ -96,6 +113,7 @@ export default function TreeMap() {
         rootNodeClassName="node__root"
         branchNodeClassName="node__branch"
         leafNodeClassName="node__leaf"
+        zoom={zoom}
         pathClassFunc={() => "node__link"}
         renderCustomNodeElement={(rd3tProps) =>
           renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
