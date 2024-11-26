b73ec3eff68f117a688b2236b35bf5b91b5fecb2
ramlow07 <luamramlow@gmail.com>
2024-11-25 22:29:24 -0300

    feat: importing SettingsProvider

--- contexts/ProviderWrapper.tsx
+++ contexts/ProviderWrapper.tsx
@@ -1,17 +1,18 @@
 "use client";
 import React, { ReactNode } from "react";
-import { ZoomProvider } from "./ZoomContext";
+import { TreemapProvider } from "./TreemapContext";
 import { NumberOfProblemsTableContextProvider } from "./NumberOfProblemsTableContext";
 import { IsModalOpenContextProvider } from "./IsModalOpenContext";
+import { SettingsProvider } from "./SettingsContext";
 
 export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
   return (
-      <ZoomProvider>
-        <NumberOfProblemsTableContextProvider>
-          <IsModalOpenContextProvider>
-            {children}
-          </IsModalOpenContextProvider>
-        </NumberOfProblemsTableContextProvider>
-      </ZoomProvider>
+    <TreemapProvider>
+      <NumberOfProblemsTableContextProvider>
+        <IsModalOpenContextProvider>
+          <SettingsProvider>{children}</SettingsProvider>
+        </IsModalOpenContextProvider>
+      </NumberOfProblemsTableContextProvider>
+    </TreemapProvider>
   );
 };
