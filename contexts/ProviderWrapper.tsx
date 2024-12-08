080eb3d190e86a5f04cfe74477f2a60b1dfdd8a5
ramlow07 <luamramlow@gmail.com>
2024-12-08 13:56:10 -0300

    feat: adding WhatsThis provider into the ProviderWrapper

--- contexts/ProviderWrapper.tsx
+++ contexts/ProviderWrapper.tsx
@@ -3,14 +3,14 @@ import React, { ReactNode } from "react";
 import { TreemapProvider } from "./TreemapContext";
 import { NumberOfProblemsTableContextProvider } from "./NumberOfProblemsTableContext";
 import { IsModalOpenContextProvider } from "./IsModalOpenContext";
-import { SettingsProvider } from "./SettingsContext";
+import { WhatsThisProvider } from "./WhatsThisContext";
 
 export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <TreemapProvider>
       <NumberOfProblemsTableContextProvider>
         <IsModalOpenContextProvider>
-          <SettingsProvider>{children}</SettingsProvider>
+          <WhatsThisProvider>{children}</WhatsThisProvider>
         </IsModalOpenContextProvider>
       </NumberOfProblemsTableContextProvider>
     </TreemapProvider>
