5509842e9eb834de7762f1624d1f93b49010f030
ramlow07 <luamramlow@gmail.com>
2024-12-08 10:19:33 -0300

    feat: adding sign in link under the textfield

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,13 +3,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Box,
-  TextField,
-  Button,
-  Container,
-  Stack,
-} from "@mui/material";
+import { Box, TextField, Button, Container, Stack } from "@mui/material";
 import { registerSchema, RegisterFormData } from "../schemas/registerSchema";
 import SignUp from "@/firebase/auth/signUp";
 import { useRouter } from "next/navigation";
@@ -25,32 +19,34 @@ export default function RegisterPage() {
     resolver: zodResolver(registerSchema),
   });
 
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const router = useRouter();
 
   async function onSubmit(data: RegisterFormData) {
-
     const { name, email, password } = data;
     const { result, error } = await SignUp(email, password);
 
-        setErrorMessage('');
-        if(error) {
-            if(error && typeof error === 'object' && 'code' in error && error.code === 'auth/email-already-in-use'){
-              setErrorMessage("Email already in use.");
-            }
-        }
-        else {
-          if(result && result.user){
-            const userRef = ref(database, 'users/' + result.user.uid);
-              await set(userRef, {
-                  name: name,
-                  email: email
-            });
-          }
-          return router.push('/login');
-        }
- 
-  };
+    setErrorMessage("");
+    if (error) {
+      if (
+        error &&
+        typeof error === "object" &&
+        "code" in error &&
+        error.code === "auth/email-already-in-use"
+      ) {
+        setErrorMessage("Email already in use.");
+      }
+    } else {
+      if (result && result.user) {
+        const userRef = ref(database, "users/" + result.user.uid);
+        await set(userRef, {
+          name: name,
+          email: email,
+        });
+      }
+      return router.push("/login");
+    }
+  }
 
   return (
     <Container maxWidth="sm" sx={{ marginTop: 8 }}>
@@ -63,7 +59,7 @@ export default function RegisterPage() {
           color: "white",
         }}
       >
-        <h1 className="text-center pb-8 text-2xl">Register.</h1>
+        <h1 className="text-center pb-8 text-2xl">Register</h1>
         <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <Stack spacing={3}>
             <TextField
@@ -105,6 +101,13 @@ export default function RegisterPage() {
                 style: { color: "white" },
               }}
             />
+            <p>
+              Already have an account?{" "}
+              <a className="text-blue-500" href="/login">
+                Click here
+              </a>{" "}
+              to sign in.
+            </p>
             <p className="p-2 text-red-500">{errorMessage}</p>
 
             <Button
