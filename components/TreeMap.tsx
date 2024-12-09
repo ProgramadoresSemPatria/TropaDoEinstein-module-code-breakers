fad0dc09d0f1d2cd7cba9b53bb720f9d51875d71
vandilsonbrito <vandilson25@outlook.com>
2024-12-09 01:30:46 -0300

    feat: add logic to sync user data with db and localstorage

--- components/TreeMap.tsx
+++ components/TreeMap.tsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useMemo, useCallback } from "react";
+'use client';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import ReactFlow, {
   Controls,
   useNodesState,
@@ -14,8 +14,15 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 import dagre from "dagre";
 
-import { initialNodes, initialEdges } from "@/utils/graphInfo";
+import { initialNodes, initialEdges } from '@/utils/graphInfo';
 import { useIsModalOpenContext } from "@/contexts/IsModalOpenContext";
+import ProgressBar from './ProgressBar';
+import { useUserInfoContext } from '@/contexts/UserInfoContext';
+import { ProgressBarInfoType, UserDataFromDBArrayType } from '@/utils/Types/types';
+import { getDataFromDB } from '@/firebase/database/functions';
+import { useAuthContext } from '@/contexts/AuthContext/AuthContext';
+//import { validateUserDataFromStorageType } from '@/utils/functions/validateUserDataFromLocalStorage';
+
 
 const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
@@ -52,7 +59,115 @@ interface NodeData {
 }
 
 export default function Graph() {
-  const { setIsPrincipalModalSectionOpen } = useIsModalOpenContext();
+
+  const { userAuth } = useAuthContext();
+  const { setIsPrincipalModalSectionOpen, isPrincipalModalSectionOpen } = useIsModalOpenContext();
+  const { setUserTotalProblemsStatusChecked } = useUserInfoContext();
+  const [progressBarInfo, setProgressBarInfo] = useState<ProgressBarInfoType[]>([{  
+    nodeId: 0,
+    progressBarValue: 0,
+    totalStatusChecked: 0
+  }]); 
+
+  const [userGraphDataFromDatabase, setUserGraphDataFromDatabase] = useState<UserDataFromDBArrayType>([]);
+
+
+  useEffect(() => {
+      if(userAuth?.uid) {
+          const fetchData = async () => { 
+            const resolvedProblems = await getDataFromDB('users/' + userAuth?.uid); 
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { name, email, ...allResolvedProblemsSaved} = resolvedProblems;
+            console.log("allResolvedProblemsSaved", allResolvedProblemsSaved)
+            if(allResolvedProblemsSaved) {
+                const data: UserDataFromDBArrayType = [];
+                for(const problemInfo in allResolvedProblemsSaved) {
+
+                  const id = (allResolvedProblemsSaved[problemInfo][0].nodeId);
+                  data.push(
+                    { 
+                      nodeId: Number(id) || 0,
+                      progressBarValue: Number(allResolvedProblemsSaved[problemInfo][0].progressBarValue) || 0,
+                      totalStatusChecked: Number(allResolvedProblemsSaved[problemInfo][0].totalStatusChecked) || 0,
+                      problemId: 0, //mock value
+                      isStarChecked: false, //mock value
+                      isStatusChecked: false, //mock value
+                    }
+                  )
+                }
+                console.log("Data", data)
+                setUserGraphDataFromDatabase(data)
+                
+            }
+          };
+          fetchData();
+          console.log("Buscouuuuuuuuuuu no DB")
+      }
+      else {
+          const storageData = JSON.parse(localStorage.getItem('user-data') ?? '{}'); 
+          
+          console.log("Buscouuuu no LOCAL-Storage")
+          //console.log("Storage", storageData)
+          const data: UserDataFromDBArrayType = [];
+          for(const problemInfo in storageData) {
+
+            const id = (storageData[problemInfo][0].nodeId);
+            data.push(
+              { 
+                nodeId: Number(id) || 0,
+                progressBarValue: Number(storageData[problemInfo][0].progressBarValue) || 0,
+                totalStatusChecked: Number(storageData[problemInfo][0].totalStatusChecked) || 0,
+                problemId: 0, //mock value
+                isStarChecked: false, //mock value
+                isStatusChecked: false, //mock value
+              }
+            )
+          }
+          setUserGraphDataFromDatabase(data)
+      }
+
+  }, [isPrincipalModalSectionOpen.id, userAuth?.uid]);
+
+  useEffect(() => {
+    if(!initialNodes || !userGraphDataFromDatabase) return;
+    
+    console.log("userGraphDataFromDatabase", userGraphDataFromDatabase)
+    setProgressBarInfo([]);
+    const newProgressBarInfo = initialNodes.map((node) => { 
+      let progressValue: number = 0;
+      let totalStatusChecked: number = 0;
+      if (userAuth?.uid) { 
+        const findResult = userGraphDataFromDatabase.find((item) => item.nodeId === Number(node.id));
+        progressValue = Number(findResult?.progressBarValue) || 0; 
+        totalStatusChecked = Number(findResult?.totalStatusChecked) || 0;  
+      } 
+      else { 
+        const findResult = userGraphDataFromDatabase.find((item) => item.nodeId === Number(node.id));
+        progressValue = Number(findResult?.progressBarValue) || 0; 
+        totalStatusChecked = Number(findResult?.totalStatusChecked) || 0; 
+      }
+
+      return { 
+        nodeId: Number(node.id) || 0, 
+        progressBarValue: progressValue,
+        totalStatusChecked: totalStatusChecked
+       };
+    });
+
+    setProgressBarInfo((prevData) => [...prevData, ...newProgressBarInfo]);
+
+  }, [initialNodes, userGraphDataFromDatabase])
+
+  useEffect(() => {
+    if(progressBarInfo) {
+      const totalChecked = progressBarInfo.reduce((acc, item) => {
+        return acc + item.totalStatusChecked;
+      }, 0)
+      setUserTotalProblemsStatusChecked(totalChecked)
+      console.log("progressBarInfo", progressBarInfo)
+      console.log("totalChecked", totalChecked)
+    }
+  }, [progressBarInfo])
 
   const handleNodeClick = (nodeId: string) => {
     setIsPrincipalModalSectionOpen({ value: true, id: Number(nodeId) });
@@ -61,42 +176,48 @@ export default function Graph() {
   // Generate initial nodes and edges with layout
   const { nodes: initialLayoutedNodes, edges: initialLayoutedEdges } = useMemo(
     () => getLayoutedElements(initialNodes, initialEdges),
-    []
+    [],
   );
 
   // State for nodes and edges
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [nodes, setNodes, onNodesChange] = useNodesState(initialLayoutedNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialLayoutedEdges);
+  
+  const nodeTypes = useMemo(() => ({
+    custom: ({ data, id }: { data: NodeData, id: string }) => {
+     
+      const hasIncoming = edges.some(edge => edge.target === id);
+      const hasOutgoing = edges.some(edge => edge.source === id);
 
-  const nodeTypes = useMemo(
-    () => ({
-      custom: ({ data, id }: { data: NodeData; id: string }) => {
-        const hasIncoming = edges.some((edge) => edge.target === id);
-        const hasOutgoing = edges.some((edge) => edge.source === id);
+      const progressValue = progressBarInfo.find((item: ProgressBarInfoType) => item.nodeId === Number(id)) ?.progressBarValue || 0;  
+      /* console.log("progressValue", progressValue === 100) */
 
-        return (
-          <div
-            className="p-4 bg-purpleLogo text-white rounded-lg flex flex-col text-center text-sm h-[62px] min-w-[200px] justify-center cursor-pointer transition ease-in-out duration-300 hover:bg-customPurpleBtn"
-            onClick={() => handleNodeClick(id)}
-          >
-            {hasIncoming && (
+      return (
+        <div
+          key={id}
+          className={`p-4 text-white rounded-lg flex flex-col text-center text-sm h-[62px] min-w-[200px] justify-center cursor-pointer transition ease-in-out duration-300 hover:bg-customPurpleBtn  progressValue ${progressValue === 100 ? 'bg-[#4a186b]' : 'bg-purpleLogo'}`}
+          onClick={() => handleNodeClick(id)}
+        >
+          { hasIncoming && (
               <Handle type="target" position={Position.Top} id="target" />
             )}
             {hasOutgoing && (
               <Handle type="source" position={Position.Bottom} id="source" />
-            )}
-            <strong>{data.label}</strong>
-          </div>
-        );
-      },
-    }),
-    [edges]
-  );
+          )}
+          <strong className="pb-2">{data.label}</strong>
+
+          <ProgressBar key={id} heightProgressBar={8} progressBarValue={Number(progressValue)}/>
+
+        </div>
+      );
+    },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }), [edges, progressBarInfo]);
 
   const edgeStyles = {
-    stroke: "var(--greenLogo)",
-    strokeWidth: 5,
-    markerEnd: "url(#arrow)",
+    stroke: 'rgb(183, 201, 196)', 
+    strokeWidth: 5,   
     smoothness: 1,
     zIndex: 10,
   };
@@ -111,15 +232,20 @@ export default function Graph() {
     <div className="w-[100vw] h-[calc(100vh-2.5rem)] lg:w-[76vw] text-white">
       <ReactFlow
         nodes={nodes}
-        edges={edges}
+        edges={edges.map(edge => ({
+          ...edge,
+          style: edgeStyles
+        }))}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
         fitView
+
       >
         <Controls />
       </ReactFlow>
     </div>
   );
-}
+  
+};
