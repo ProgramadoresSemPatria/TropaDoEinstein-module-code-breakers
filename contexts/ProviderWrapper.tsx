91398e1e908919933489081bf837871fb65030d9
vandilsonbrito <vandilson25@outlook.com>
2024-12-09 01:21:33 -0300

    feat: add 'WhatsThisProvider' and remove 'SettingsProvider'

--- contexts/ProviderWrapper.tsx
+++ contexts/ProviderWrapper.tsx
@@ -3,29 +3,17 @@ import React, { ReactNode } from "react";
 import { NumberOfProblemsTableContextProvider } from "./NumberOfProblemsTableContext";
 import { IsModalOpenContextProvider } from "./IsModalOpenContext";
 import { WhatsThisProvider } from "./WhatsThisContext";
-
-export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
-  return (
-    <TreemapProvider>
-      <NumberOfProblemsTableContextProvider>
-        <IsModalOpenContextProvider>
-          <WhatsThisProvider>{children}</WhatsThisProvider>
-        </IsModalOpenContextProvider>
-      </NumberOfProblemsTableContextProvider>
-    </TreemapProvider>
-    
-import { SettingsProvider } from "./SettingsContext";
 import { AuthContextProvider } from "./AuthContext/AuthProvider";
 import { UserInfoContextProvider } from "./UserInfoContext";
 
-
+    
 export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <AuthContextProvider>
         <UserInfoContextProvider>
             <NumberOfProblemsTableContextProvider>
               <IsModalOpenContextProvider>
-                <SettingsProvider>{children}</SettingsProvider>
+                <WhatsThisProvider>{children}</WhatsThisProvider>
               </IsModalOpenContextProvider>
             </NumberOfProblemsTableContextProvider>
         </UserInfoContextProvider>
